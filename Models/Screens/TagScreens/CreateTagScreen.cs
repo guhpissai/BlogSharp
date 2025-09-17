8a036d06cfbe93e12e33f4bcc9f53056c4492947
guhpissai <gustavopissai3d@gmail.com>
2025-09-17 18:03:43 -0300

    Implementando tela responsavel por criar uma nova Tag (CreateTagScreen)

--- Models/Screens/TagScreens/CreateTagScreen.cs
+++ Models/Screens/TagScreens/CreateTagScreen.cs
@@ -1,9 +1,45 @@
+using Blog.Models;
+using Blog.Repositories;
+using Dapper.Contrib.Extensions;
+
 namespace Blog.Screens.TagScreens
 {
   public static class CreateTagScreen
   {
     public static void Load()
     {
+      Console.Clear();
+      Console.WriteLine("========= Criar Tag =========");
+
+      Console.Write("Qual vai ser o nome da tag? ");
+      var name = Console.ReadLine();
+      var slug = name.ToLower();
+
+      var tag = new Tag();
+      tag.Name = name;
+      tag.Slug = slug;
+
+      try
+      {
+        var repository = new Repository<Tag>(Database.Connection);
+        repository.Create(tag);
+
+        Console.WriteLine($"Tag {name} criada com sucesso!");
+
+        Console.WriteLine();
+        Console.WriteLine("Pressione ENTER para voltar...");
+        Console.ReadKey();
+        MenuTagScreen.Load();
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("\n❌ Não foi possível criar a Tag, pressione ENTER para tentar novamente...");
+        Console.WriteLine("");
+        Console.WriteLine(ex.Message);
+        Console.ReadKey();
+        Console.Clear();
+        Load();
+      }
     }
   }
 }
--- Models/Screens/TagScreens/ListTagScreen.cs
+++ Models/Screens/TagScreens/ListTagScreen.cs
@@ -16,11 +16,23 @@ namespace Blog.Screens.TagScreens
 
       var tags = repository.Get();
 
-      foreach (var tag in tags)
+      Console.Clear();
+      Console.WriteLine("============ LISTA DE TAGS ==============");
+      Console.WriteLine();
+
+      if (tags == null)
       {
-        Console.WriteLine($"{tag.Id} - {tag.Name} ({tag.Slug})");
+        Console.WriteLine("Nenhuma tag encontrada");
+      }
+      else
+      {
+        foreach (var tag in tags)
+        {
+          Console.WriteLine($"{tag.Name.ToString().PadRight(15)} | {tag.Slug}");
+        }
       }
 
+      Console.WriteLine();
       Console.WriteLine("Pressione ENTER para voltar...");
       Console.ReadKey();
       Program.Menu();
